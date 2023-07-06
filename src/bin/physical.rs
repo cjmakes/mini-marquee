3d657f18876a1abef315a0a06aa017febac69e58
conjones <cj@cjmakes.com>
2023-07-05 20:23:18 -0400

    move bin

--- src/bin/physical.rs
+++ src/bin/physical.rs
@@ -1,8 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::iter::once;
-
+use embedded_graphics::mono_font::ascii::FONT_10X20;
 use embedded_hal::timer::CountDown;
 use fugit::ExtU32;
 use fugit::RateExtU32;
@@ -20,11 +19,13 @@ use waveshare_rp2040_zero::{
     Pins, XOSC_CRYSTAL_FREQ,
 };
 
-use smart_leds::{brightness, SmartLedsWrite, RGB8};
-use ws2812_pio::Ws2812;
-
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -57,15 +58,6 @@ fn main() -> ! {
 
     // Configure the addressable LED
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-    let mut ws = Ws2812::new(
-        // The onboard NeoPixel is attached to GPIO pin #16 on the Feather RP2040.
-        pins.neopixel.into_mode(),
-        &mut pio,
-        sm0,
-        clocks.peripheral_clock.freq(),
-        timer.count_down(),
-    );
-
     let i2c = i2c::I2C::i2c1(
         pac.I2C1,
         pins.gp14.into_mode(), // sda
@@ -80,50 +72,26 @@ fn main() -> ! {
     let display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0);
     let mut display = display.into_buffered_graphics_mode();
     display.init().unwrap();
+    let mut delay = timer.count_down();
 
     let mut pos = 0;
+    let style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
     loop {
-        if pos == 4096 {
+        if pos == -260 {
             pos = 0;
-            display.clear_buffer();
-            ws.write(brightness(once(RGB8 { r: 255, g: 0, b: 0 }), 10))
-                .unwrap();
-            delay.start(500.millis());
-            let _ = nb::block!(delay.wait());
-
-            ws.write(brightness(once(RGB8 { r: 0, g: 255, b: 0 }), 10))
-                .unwrap();
-            delay.start(500.millis());
-            let _ = nb::block!(delay.wait());
-
-            ws.write(brightness(once(RGB8 { r: 0, g: 0, b: 255 }), 10))
-                .unwrap();
-            delay.start(500.millis());
-            let _ = nb::block!(delay.wait());
         }
-
-        Pixel(Point::new(pos / 32, pos % 32), BinaryColor::On)
-            .draw(&mut display)
-            .unwrap();
-
+        display.clear_buffer();
+        Text::new(
+            "hi caroline, i love you <3",
+            Point::new(pos, 20),
+            style,
+        )
+        .draw(&mut display)
+        .unwrap();
+        pos -= 1;
         display.flush().unwrap();
 
-        pos += 1;
-        delay.start(10.millis());
+        delay.start(5.millis());
         let _ = nb::block!(delay.wait());
-
-        if pos % 32 == 0 {
-            pos += 32;
-
-            ws.write(brightness(once(RGB8 { r: 255, g: 0, b: 0 }), 10))
-                .unwrap();
-            delay.start(100.millis());
-            let _ = nb::block!(delay.wait());
-
-            ws.write(brightness(once(RGB8 { r: 0, g: 0, b: 255 }), 10))
-                .unwrap();
-            delay.start(100.millis());
-            let _ = nb::block!(delay.wait());
-        }
     }
 }
