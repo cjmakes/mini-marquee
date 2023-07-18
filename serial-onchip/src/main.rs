848beca105401726db0b984331b7c669de432f4b
conjones <cj@cjmakes.com>
2023-07-18 01:29:30 -0400

    Switches serial-onchip to use interrupt instead of polling

--- serial-onchip/src/main.rs
+++ serial-onchip/src/main.rs
@@ -6,6 +6,7 @@ use panic_halt as _;
 
 use waveshare_rp2040_zero::entry;
 use waveshare_rp2040_zero::{hal, pac, Pins, XOSC_CRYSTAL_FREQ};
+use waveshare_rp2040_zero::hal::pac::interrupt;
 
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_serial::SerialPort;
@@ -15,11 +16,19 @@ use ssd1306::{
 };
 
 use embedded_graphics::{
-    mono_font::{ascii::FONT_10X20, MonoTextStyle},
     pixelcolor::BinaryColor,
     prelude::*,
-    text::Text,
 };
+
+// Globals
+/// The USB Device Driver (shared with the interrupt).
+static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+/// The USB Bus Driver (shared with the interrupt).
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+/// The USB Serial Device Driver (shared with the interrupt).
+static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+static mut DISPLAY_TEXT: [u8; 256] = [0x20; 256];
+static mut DISPLAY_TEXT_LEN: usize = 0;
 
 #[entry]
 fn main() -> ! {
@@ -64,8 +73,6 @@ fn main() -> ! {
     let display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0);
     let mut display = display.into_buffered_graphics_mode();
     display.init().unwrap();
-
-    let style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
     display.clear_buffer();
     display.flush().unwrap();
 
@@ -77,34 +84,59 @@ fn main() -> ! {
         true,
         &mut pac.RESETS,
     ));
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_BUS = Some(usb_bus);
+    }
+    // Grab a reference to the USB Bus allocator. We are promising to the
+    // compiler not to take mutable access to this global variable whilst this
+    // reference exists!
+    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
 
     // Set up the USB Communications Class Device driver
-    let mut serial = SerialPort::new(&usb_bus);
+    let serial = SerialPort::new(bus_ref);
+    unsafe {
+        USB_SERIAL = Some(serial);
+    }
 
     // Create a USB device with a fake VID and PID
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0xcafe, 0x27dd))
+    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0xcafe, 0x0001))
         .manufacturer("conjones")
         .product("mini-marquee")
         .serial_number("0x01")
         .device_class(2) // from: https://www.usb.org/defined-class-codes
         .build();
+    unsafe {
+        // Note (safety): This is safe as interrupts haven't been started yet
+        USB_DEVICE = Some(usb_dev);
+    }
 
-    let _timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    // Enable the USB interrupt
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
 
+    let mut t = 0i32;
     loop {
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-            match serial.read(&mut buf) {
-                Err(_) | Ok(0) => {}
-                Ok(count) => {
-                    display.clear(BinaryColor::Off).unwrap();
-                    let buf = core::str::from_utf8_mut(&mut buf[..count]).unwrap();
-                    Text::new(buf, Point::new(0, 20), style)
-                        .draw(&mut display)
-                        .unwrap();
-                    display.flush().unwrap();
-                }
-            }
+        t += 2;
+        display.clear(BinaryColor::Off).unwrap();
+        mini_marquee::draw_frame(&mut display, t, unsafe {core::str::from_utf8_mut(&mut DISPLAY_TEXT[..DISPLAY_TEXT_LEN]).unwrap()});
+        display.flush().unwrap();
+    }
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    // Grab the global objects. This is OK as we only access them under interrupt.
+    let usb_dev = USB_DEVICE.as_mut().unwrap();
+    let serial = USB_SERIAL.as_mut().unwrap();
+
+    // Poll the USB driver with all of our supported USB Classes
+    if usb_dev.poll(&mut [serial]) {
+        match serial.read(&mut DISPLAY_TEXT) {
+            Err(_) | Ok(0) => {}
+            Ok(count) => {DISPLAY_TEXT_LEN = count}
         }
     }
 }
